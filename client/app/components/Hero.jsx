63d8de1afac04740b6b5cbbe9d4608cd5d120c12
Okechukwu Chidera <okechukwuchidera12@gmail.com>
2024-10-28 19:20:01 +0100

    started working on the client side

---
+++ client/app/components/Hero.jsx
@@ -0,0 +1,64 @@
+import Link from 'next/link';
+import React from 'react';
+import heroImage from '../assets/heroImageb.png';
+import heroImageMobile from '../assets/heroImageMobileb.png';
+import Image from 'next/image';
+
+export default function Hero() {
+  const stats = [
+    {
+      number: '200+',
+      text: 'International Brands',
+    },
+    {
+      number: '2,000+',
+      text: 'High-Quality Products',
+    },
+    {
+      number: '30,000+',
+      text: 'Happy Customers',
+    },
+  ];
+  return (
+    <section className="lg:flex max-h-[700]">
+      <div className="px-4">
+        <h2 className=" font-black mb-3 text-4xl">
+          FIND CLOTHES THAT MATCHES YOUR STYLE
+        </h2>
+        <p className=" mb-3 text-xs md:text-base text-slate-500">
+          Browse through our diverse range of meticulously crafted garments,
+          designed to bring out your individuality and carter to your sense of
+          style
+        </p>
+        <Link
+          className="block mx-auto lg:mx-0 mb-4 bg-black  py-2 w-full lg:w-fit lg:px-10 rounded-full text-white text-sm text-center"
+          href="#"
+        >
+          Shop Now
+        </Link>
+        <div className="flex flex-wrap justify-center gap-x-10 gap-y-2">
+          {stats.map((stat, i) => (
+            <div key={i}>
+              <h3 className="text-black lg:text-2xl lg:font-semibold font-extrabold">
+                {stat.number}
+              </h3>
+              <p className="text-xs text-slate-500">{stat.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <Image
+        className="lg:hidden"
+        src={heroImageMobile}
+        width={1440}
+        height={663}
+      />
+      <Image
+        className="hidden bg-inherit lg:block"
+        src={heroImage}
+        width={2000}
+        height={663}
+      />
+    </section>
+  );
+}
--- client/app/components/Navbar.jsx
+++ client/app/components/Navbar.jsx
@@ -1,24 +1,120 @@
+'use client';
 import Image from 'next/image';
 import userIcon from '../assets/userIcon.svg';
 import burgerMenu from '../assets/BurgerMenu.svg';
 import cartIcon from '../assets/cartIcon.svg';
 import searchIcon from '../assets/searchIcon.svg';
+import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Navbar() {
+  const [openMenu, setOpenMenu] = useState(false);
+  const navMenu = [
+    {
+      name: 'Shop',
+      href: '#',
+    },
+    {
+      name: 'On Sale',
+      href: '#',
+    },
+    {
+      name: 'New Arrivals',
+      href: '#',
+    },
+    {
+      name: 'Brands',
+      href: '#',
+    },
+  ];
+  const handleMenuOpen = () => setOpenMenu(true);
+  const handleMenuClose = () => setOpenMenu(false);
+
   return (
-    <nav className="bg-white w-full justify-between h-fit py-2 px-6 flex md:hidden">
-      <div className="flex justify-center items-center space-x-4">
-        <Image src={burgerMenu} alt="burgerMenu" width={20} height={20} />
+    <>
+      <nav className="bg-white w-full justify-between h-fit py-2 px-6 flex lg:hidden ">
+        {/* MOBILE NAVIGATION */}
+
+        <div className="flex items-center">
+          <button className="mr-4" onClick={handleMenuOpen}>
+            <Image src={burgerMenu} alt="burgerMenu" width={20} height={20} />
+          </button>
+          {openMenu && (
+            <div className="absolute top-0 text-black h-full px-6 pt-10 left-0  bg-slate-100">
+              <span className="cursor-pointer" onClick={handleMenuClose}>
+                x
+              </span>
+              <ul className="flex flex-col justify-center text-black space-y-4 items-center">
+                {navMenu.map((el, i) => (
+                  <li className="cursor-pointer" key={i}>
+                    <Link href={el.href}>{el.name}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+          <h1 className="text-black font-sans font-extrabold text-3xl ">
+            ODECOHUB
+          </h1>
+        </div>
+
+        <div className="flex  space-x-5 ">
+          <Image
+            className="cursor-pointer"
+            src={searchIcon}
+            width={20}
+            height={20}
+            alt="searchIcon"
+          />
+          <Image
+            className="cursor-pointer"
+            src={cartIcon}
+            alt="add to cart"
+            width={20}
+            height={20}
+          />
+          <Image
+            className="cursor-pointer"
+            src={userIcon}
+            alt="profile"
+            width={20}
+            height={20}
+          />
+        </div>
+      </nav>
+
+      {/* DESKTOP NAV */}
+      <nav className=" hidden lg:flex items-center justify-between bg-white w-full  h-fit py-2 px-6 relative">
         <h1 className="text-black font-sans font-extrabold text-3xl ">
           ODECOHUB
         </h1>
-      </div>
+        <ul className="flex text-black space-x-2 items-center">
+          {navMenu.map((el, i) => (
+            <li key={i}>
+              <Link href={el.href}>{el.name}</Link>
+            </li>
+          ))}
+        </ul>
+        <div className="relative  max-w-md">
+          <input
+            type="text"
+            placeholder="Search for products..."
+            className=" text-black bg-slate-200 w-full pl-10 pr-4 py-2 border rounded-full focus:outline-none focus:ring-slate-300 focus:border-transparent"
+          />
+          <Image
+            src={searchIcon}
+            width={20}
+            height={20}
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 "
+          />
+        </div>
 
-      <div className="flex border-2 space-x-3 border-black">
-        <Image src={searchIcon} width={20} height={20} alt="searchIcon" />
-        <Image src={cartIcon} alt="add to cart" width={20} height={20} />
-        <Image src={userIcon} alt="profile" width={20} height={20} />
-      </div>
-    </nav>
+        <div className="flex  space-x-5 ">
+          <Image src={cartIcon} alt="add to cart" width={20} height={20} />
+          <Image src={userIcon} alt="profile" width={20} height={20} />
+        </div>
+      </nav>
+    </>
   );
 }
